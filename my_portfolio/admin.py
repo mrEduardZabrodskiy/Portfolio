3eee31753d89e22a6bce4eacfeed86c32084e2ee
Eduard <eduard.zabrodskiy@gmail.com>
2024-07-19 16:12:50 +0200

    fix models, add horizontal filter for model-admin

--- my_portfolio/admin.py
+++ my_portfolio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Person, HardSkill, SoftSkill, Experience, Project
 
 # Register your models here.
+class ProjectSkills(admin.ModelAdmin):
+    filter_horizontal = ('tools',)
+
+
+admin.site.register(Person)
+admin.site.register(HardSkill)
+admin.site.register(SoftSkill)
+admin.site.register(Experience)
+admin.site.register(Project, ProjectSkills)
+
+
--- my_portfolio/models.py
+++ my_portfolio/models.py
@@ -11,24 +11,35 @@ class HardSkill(models.Model):
     ]
     title = models.CharField(max_length=150)
     level = models.CharField(choices=LEVEL_CHOICES, default='TR', max_length=2)
+    
+    
+    def __str__(self):
+        return self.title
 
 
 class SoftSkill(models.Model):
     title = models.CharField(max_length=100)
     skill_level = models.CharField(max_length=100, blank=True)
+    
+    def __str__(self):
+        return self.title
 
 
 class Project(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField(max_length=3000, blank=True)
     tools = models.ManyToManyField(HardSkill, related_name='projects', blank=True)
-    url = models.URLField()
-    repository_url = models.URLField()
+    url = models.URLField(blank=True)
+    repository_url = models.URLField(blank=True)
     created = models.DateField(auto_now=True)
     
     
     class Meta:
         ordering = ['-created']
+        
+        
+    def __str__(self):
+        return self.title
         
 
 class Experience(models.Model):
@@ -38,6 +49,9 @@ class Experience(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     
+    
+    def __str__(self):
+        return self.title
 
 class Person(models.Model):
     first_name = models.CharField(max_length=150)
@@ -50,3 +64,6 @@ class Person(models.Model):
     education = models.CharField(max_length=300, blank=True)
     info_about_person = models.TextField(max_length=4000, blank=True)
     
+    
+    def __str__(self):
+        return self.first_name
